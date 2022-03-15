278808461dc2120816aafbb0dda11608393ef771
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-03-15 21:55:05 +0530

    Page Layout for Data Info

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Card,CardHeader,CardContent,Typography,Grid,Divider} from '@material-ui/core';
+
+import useStyles from './styles';
+
+const Main = () => {
+
+    const classes=useStyles();
+    return (
+        <Card className={classes.root}>
+            <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+
+            <CardContent>
+                <Typography align="center" variant="h5">Total Balance $100</Typography>
+                <Typography ariant="subtitle1" style={{lineHeight: '1.5rem',marginTop:'20px'}}>
+                    {/* */}
+                    Try Saying
+                </Typography>
+                <Divider />
+
+                {/* Form */}
+            </CardContent>
+
+            <CardContent className={classes.cardContent}>
+            <Grid container spacing ={2}>
+                  <Grid item cs={12}>
+                    {/*  <List />*/}
+                  </Grid>
+            </Grid>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Main
