f8d5bf646e593303e457cda3c4235f018bd109c9
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-03-16 09:47:13 +0530

    context

--- src/components/Main/List/styles.js
+++ src/components/Main/List/styles.js
@@ -1,17 +1,9 @@
-import { makeStyles } from '@material-ui/core/styles';
-import { red, green } from '@material-ui/core/colors';
+import React from 'react'
 
-export default makeStyles((theme) => ({
-  avatarIncome: {
-    color: '#fff',
-    backgroundColor: green[500],
-  },
-  avatarExpense: {
-    color: theme.palette.getContrastText(red[500]),
-    backgroundColor: red[500],
-  },
-  list: {
-    maxHeight: '150px',
-    overflow: 'auto',
-  },
-}));
+const styles = () => {
+  return (
+    <div>styles</div>
+  )
+}
+
+export default styles
